8aa75019c3ba0b3295aa05eda8a976d8b5271697
WinHo <dovanthang118@gmail.com>
2024-01-30 20:09:47 +0700

    new

--- exam/exam_flutter/lib/home_page.dart
+++ exam/exam_flutter/lib/home_page.dart
@@ -2,6 +2,8 @@ import 'package:exam_flutter/tabbar_example.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
+import 'item_widget.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -17,48 +19,116 @@ class _HomePageState extends State<HomePage> {
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "Hi Guy !",
-              style: TextStyle(color: Colors.white, fontSize: 20),
+          body: AppBarContainer(
+            titleString: 'home',
+            title: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kItemPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Hi Guy! ',
+                        style: TextStyles.defaultStyle.fontHeader.whiteTextColor.bold,
+                      ),
+                      const SizedBox(
+                        height: kMediumPadding,
+                      ),
+                      Text(
+                        'Where are you going next?',
+                        style: TextStyles.defaultStyle.fontCaption.whiteTextColor,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-            backgroundColor: Colors.blue.withOpacity(0.8),
-          ),
-          body: IndexedStack(
-            index: _currentIndex,
-            children: [
-              Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp4rN33PyWHKh1AnV91DHPLsB-t-FEAjS2eA&usqp=CAU"),
-              TabBarExample(),
-            ],
-          )
-          ,
-          bottomNavigationBar: SalomonBottomBar(
-            currentIndex: _currentIndex,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-            items: [
-              SalomonBottomBarItem(
-                icon: const Icon(Icons.home),
-                title: const Text("Home"),
-              ),
-              SalomonBottomBarItem(
-                  icon: const Icon(Icons.search),
-                  title: const Text("Search")
-              ),
-              SalomonBottomBarItem(
-                  icon: const Icon(Icons.settings),
-                  title: const Text("Setting")
-              ),
-              SalomonBottomBarItem(
-                  icon: const Icon(Icons.person),
-                  title: const Text("User")
-              ),
-            ],
+            implementLeading: false,
+            child: Column(
+              children: [
+                TextField(
+                  enabled: true,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                    hintText: 'Search your destination',
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        FontAwesomeIcons.magnifyingGlass,
+                        color: Colors.black,
+                        size: 14,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          kItemPadding,
+                        ),
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: kItemPadding),
+                  ),
+                  style: TextStyles.defaultStyle,
+                  onChanged: (value) {},
+                  onSubmitted: (String submitValue) {},
+                ),
+                const SizedBox(
+                  height: kDefaultPadding,
+                ),
+                const SizedBox(
+                  height: kMediumPadding,
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ItemWidget( imagePath: 'assets/images/ico_hotel.png', backgroundColor: Color(0xFFE0A587),),
+                      ItemWidget( imagePath: 'assets/images/ico_hotel_plane.png', backgroundColor: Color(0xFFA95E5E),),
+                      ItemWidget( imagePath: 'assets/images/ico_plane.png', backgroundColor: Color(0xFF8FD9CF),),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Popular Destinations',
+                      style: TextStyles.defaultStyle.bold,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: kMediumPadding,
+                ),
+                Expanded(
+                    child: CustomScrollView(
+                      primary: false,
+                      slivers: <Widget>[
+                        SliverPadding(
+                          padding: const EdgeInsets.all(0),
+                          sliver: SliverGrid.count(
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            crossAxisCount: 2,
+                            children: listImageLeft
+                                .map(
+                                  (e) => _buildImageHomScreen(
+                                e['name']!,
+                                e['image']!,
+                              ),
+                            ).toList(),
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+              ],
+            ),
           ),
         ),
       ),
